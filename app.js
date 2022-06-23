d668e9d2c996b782d878cb7ccd9735f884c08fcf
Hao Hao <architec@funkthetown.net>
2022-06-23 19:59:31 +0000

    Funkthetown V4

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ module.exports = app;
 const bodyParser = require ('body-parser');
 const path = require('path');
 
+
 // const env = require(`/environment/${process.env.NODE_ENV}`);
 
 
@@ -23,47 +24,47 @@ app.use(bodyParser.json())
 
 //demo
 
-// const host = '0.0.0.0';
-// const port = process.env.PORT || 3000;
+  const host = '0.0.0.0';
+  const port = process.env.PORT || 3000;
 
-// app.listen(port, host, function(){
+  app.listen(port, host, function(){
 
-//     console.log("Server started");
-// });
+     console.log("Server started");
+  });
 
 //live
 
- const httpServer = http.createServer((req,res)=>{
-   res.writeHead(301,{Location:`https://${req.headers.host}${req.url}`});
-   res.end();
- }).listen(80);
+  // const httpServer = http.createServer((req,res)=>{
+  //  res.writeHead(301,{Location:`https://${req.headers.host}${req.url}`});
+  //   res.end();
+  // }).listen(80);
 
- const httpsServer = https.createServer({
-   key: fs.readFileSync('/etc/letsencrypt/live/www.funkthetown.net/privkey.pem'),
-   cert: fs.readFileSync('/etc/letsencrypt/live/www.funkthetown.net/fullchain.pem'),
- }, app).listen(443);
-
-
+  // const httpsServer = https.createServer({
+  //   key: fs.readFileSync('/etc/letsencrypt/live/www.funkthetown.net/privkey.pem'),
+  //   cert: fs.readFileSync('/etc/letsencrypt/live/www.funkthetown.net/fullchain.pem'),
+  // }, app).listen(443);
 
 
-app.set('view engine','ejs');
 
+app.engine('html', require('ejs').renderFile);
+ app.set('view engine','html');
+ app.set('views', path.join(__dirname, '/'));
 
 
 app.post('/', function (req,res){
-    
+
   addEmailToMailchimp(req.body.email);
-    
+
     // const confirm = modal() ;
-          // setTimeout(function(){
+           setTimeout(function(){
 
             res.render('index', function (err, html) {
-              res.send(html)
-            })
-            
+               res.send(html)
+             })
 
 
-  // },3000);
+
+   },2000);
 
   })
 
@@ -84,13 +85,13 @@ function addEmailToMailchimp(email) {
       'Cookie': 'ak_bmsc=8D2C3706EC60AAB075D659E37EE2B1BF02167195890500003E4D775F99991B13~pl5BiFaLOkE1BOZ+FhGRNLP/D5q+WNADkV/IItWN3E+FMwcSrxhqlHVypec9KZmGWupO9ImTL1+bntdNXIs31HVBoQRKlj7JLp90Ma7CsmQCESU76huWnIuWHKYGKkGaVRQdIVA6kZHiHF/zzMlyg+Z9pGvqL0imCJigjbSGKmc8VFmmsdWLXIBg3J6AWxaDjZR9ICAfMvZlGvUoLMGU+3+WTRcRSWO+X8s5wVJRq6Xio=; bm_sv=5B372C21AA2A1D88B00B74CE35B527F3~oaVEkp6P1a0srXYDUOT8aph9Gxg0NQdgUtCA/N5EGFQwOstM/cM4lenzlFCe+cbZ+8pDM/tylTX2EDXrzP5DWd0G2pOwS98fcZchfOuEBIGCeWI6Tfs99Vxo6HOLai5GuuC6smMnrxLlkULXZEWAwsJK1Y5SGev6yoUd4hmTSrw='
     },
     body: JSON.stringify({"email_address":email,"status":"subscribed"})
-  
+
   };
   request(options, function (error, response) {
     if (error) throw new Error(error);
     console.log(response.body);
   });
-  
+
 
 }
 
