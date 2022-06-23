7f0192bcf2edff309f2d54e542c7256d0ed0ecb7
Hao Hao <architec@funkthetown.net>
2022-06-23 21:12:19 +0000

    Funkthetown V4 update 6

--- app.js
+++ app.js
@@ -22,9 +22,9 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 
- app.engine('html', require('ejs').renderFile);
- app.set('view engine','html');
- app.set('views', path.join(__dirname, '/'));
+//  app.engine('html', require('ejs').renderFile);
+//  app.set('view engine','html');
+//  app.set('views', path.join(__dirname, '/'));
 
 
 //demo
@@ -53,48 +53,48 @@ app.use(bodyParser.json())
 
 
 
-app.post('/', function (req,res){
+// app.post('/', function (req,res){
 
-  addEmailToMailchimp(req.body.email);
+//   addEmailToMailchimp(req.body.email);
 
-    // const confirm = modal() ;
-           setTimeout(function(){
+//     // const confirm = modal() ;
+//            setTimeout(function(){
 
-            res.render('index', function (err, html) {
-               res.send(html)
-             })
+//             res.render('index', function (err, html) {
+//                res.send(html)
+//              })
 
 
 
-   },2000);
+//    },2000);
 
-  })
+//   })
 
 
   // sendFile(__dirname+'/index.html');
 
 
 
-function addEmailToMailchimp(email) {
+// function addEmailToMailchimp(email) {
 
-  var request = require('request');
-  var options = {
-    'method': 'POST',
-    'url': 'https://us2.api.mailchimp.com/3.0/lists/8af872df3c/members',
-    'headers': {
-      'Authorization': 'Basic aGFvOmM4OTk2M2EzOThlODE1ZGQ1NDhmMzBjZTQ4ODUyOTI2LXVzMg==',
-      'Content-Type': 'application/json',
-      'Cookie': 'ak_bmsc=8D2C3706EC60AAB075D659E37EE2B1BF02167195890500003E4D775F99991B13~pl5BiFaLOkE1BOZ+FhGRNLP/D5q+WNADkV/IItWN3E+FMwcSrxhqlHVypec9KZmGWupO9ImTL1+bntdNXIs31HVBoQRKlj7JLp90Ma7CsmQCESU76huWnIuWHKYGKkGaVRQdIVA6kZHiHF/zzMlyg+Z9pGvqL0imCJigjbSGKmc8VFmmsdWLXIBg3J6AWxaDjZR9ICAfMvZlGvUoLMGU+3+WTRcRSWO+X8s5wVJRq6Xio=; bm_sv=5B372C21AA2A1D88B00B74CE35B527F3~oaVEkp6P1a0srXYDUOT8aph9Gxg0NQdgUtCA/N5EGFQwOstM/cM4lenzlFCe+cbZ+8pDM/tylTX2EDXrzP5DWd0G2pOwS98fcZchfOuEBIGCeWI6Tfs99Vxo6HOLai5GuuC6smMnrxLlkULXZEWAwsJK1Y5SGev6yoUd4hmTSrw='
-    },
-    body: JSON.stringify({"email_address":email,"status":"subscribed"})
+//   var request = require('request');
+//   var options = {
+//     'method': 'POST',
+//     'url': 'https://us2.api.mailchimp.com/3.0/lists/8af872df3c/members',
+//     'headers': {
+//       'Authorization': 'Basic aGFvOmM4OTk2M2EzOThlODE1ZGQ1NDhmMzBjZTQ4ODUyOTI2LXVzMg==',
+//       'Content-Type': 'application/json',
+//       'Cookie': 'ak_bmsc=8D2C3706EC60AAB075D659E37EE2B1BF02167195890500003E4D775F99991B13~pl5BiFaLOkE1BOZ+FhGRNLP/D5q+WNADkV/IItWN3E+FMwcSrxhqlHVypec9KZmGWupO9ImTL1+bntdNXIs31HVBoQRKlj7JLp90Ma7CsmQCESU76huWnIuWHKYGKkGaVRQdIVA6kZHiHF/zzMlyg+Z9pGvqL0imCJigjbSGKmc8VFmmsdWLXIBg3J6AWxaDjZR9ICAfMvZlGvUoLMGU+3+WTRcRSWO+X8s5wVJRq6Xio=; bm_sv=5B372C21AA2A1D88B00B74CE35B527F3~oaVEkp6P1a0srXYDUOT8aph9Gxg0NQdgUtCA/N5EGFQwOstM/cM4lenzlFCe+cbZ+8pDM/tylTX2EDXrzP5DWd0G2pOwS98fcZchfOuEBIGCeWI6Tfs99Vxo6HOLai5GuuC6smMnrxLlkULXZEWAwsJK1Y5SGev6yoUd4hmTSrw='
+//     },
+//     body: JSON.stringify({"email_address":email,"status":"subscribed"})
 
-  };
-  request(options, function (error, response) {
-    if (error) throw new Error(error);
-    console.log(response.body);
-  });
+//   };
+//   request(options, function (error, response) {
+//     if (error) throw new Error(error);
+//     console.log(response.body);
+//   });
 
 
-}
+// }
 
 
