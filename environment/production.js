1ed8174104e21f040a9bf111dcc43872e00c3803
Hao Hao <architec@funkthetown.net>
2022-11-13 13:03:55 +0000

    Funkthetown V4 update 12

--- environment/production.js
+++
@@ -1,7 +0,0 @@
-// module.exports = {
-
-//     // dbUrl: ,
-//     cert: '/etc/letsencrypt/live/www.funkthetown.net/fullchain.pem',
-//     key: '/etc/letsencrypt/live/www.funkthetown.net/privkey.pem'
-
-// }
